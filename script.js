d812d247b09d083788162991de3611723d0d11a6
aaliyahSHF <aaliyah@skyhighfarm.org>
2026-08-12 11:50:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ L.tileLayer(
 attribution:'© OpenStreetMap'
 }
 ).addTo(map);
+
+
+
+L.marker([41.986,-73.619])
+.addTo(map)
+.bindPopup(
+"<b>Main Herd</b><br>" +
+"North Pasture<br>" +
+"August 5, 2026<br>" +
+"8:00 AM"
+)
+.openPopup();
