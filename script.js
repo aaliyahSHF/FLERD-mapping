b20c20c72f68523364dcdfa993dcffe52fa52e3a
aaliyahSHF <aaliyah@skyhighfarm.org>
2026-08-12 12:02:15 -0400

    Update script.js
    Redo

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-var map = L.map('map').setView(
-[41.986,-73.619],
-25
+// ========================================
+// PASTURE TRACKING MAP
+// ========================================
+
+// SHF coordinates
+const farmLocation = [42.001472, -73.639056];
+
+// ----------------------------------------
+// BASE MAPS
+// ----------------------------------------
+
+// Standard street map
+const streetMap = L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: '&copy; OpenStreetMap contributors'
+    }
 );
 
+// Topographic / terrain map
+const terrainMap = L.tileLayer(
+    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    {
+        maxZoom: 17,
+        attribution:
+            'Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap'
+    }
+);
 
-L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-attribution:'© OpenStreetMap'
-}
-).addTo(map);
+// ----------------------------------------
+// CREATE MAP
+// ----------------------------------------
 
+const map = L.map('map', {
+    center: farmLocation,
+    zoom: 16,
+    layers: [terrainMap]
+});
 
+// ----------------------------------------
+// MAP LAYER SWITCHER
+// ----------------------------------------
 
-L.marker([41.986,-73.619])
-.addTo(map)
-.bindPopup(
-"<b>Main Herd</b><br>" +
-"North Pasture<br>" +
-"August 5, 2026<br>" +
-"8:00 AM"
-)
-.openPopup();
+const baseMaps = {
+    "Terrain": terrainMap,
+    "Street Map": streetMap
+};
+
+L.control.layers(baseMaps).addTo(map);
+
+// ----------------------------------------
+// FARM LOCATION MARKER
+// ----------------------------------------
+
+const farmMarker = L.marker(farmLocation).addTo(map);
+
+farmMarker.bindPopup(`
+    <strong>Home Farm</strong><br>
+    Pasture Tracking Map
+`).openPopup();
