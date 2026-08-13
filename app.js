c6b634e0a05c9170fc65b6b59c0c29d07cdf3420
aaliyahSHF <aaliyah@skyhighfarm.org>
2026-08-13 15:35:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const CONFIG = {
 
+  MAP_IMAGE:
+    "assets/SHF map.png",
+
+  LOCAL_CSV:
+    "data/flerd-log.csv",
+
+  /*
+    PASTE YOUR GOOGLE APPS SCRIPT
+    /exec URL HERE.
+  */
+  APPS_SCRIPT_URL:
+    "",
+
+  DATA_YEAR: 2026,
+
+  IMAGE_WIDTH: 724,
+
+  IMAGE_HEIGHT: 582,
+
+  PASTURE_FILL_OPACITY: 0.12,
+
+  PASTURE_WEIGHT: 2.5,
+
+  /*
+    Google Sheet is checked every
+    15 seconds.
+  */
+  SYNC_INTERVAL_MS: 15000,
+
+  SHOW_APPROXIMATE_OLD_CSV_POINTS:
+    true
+
+};
